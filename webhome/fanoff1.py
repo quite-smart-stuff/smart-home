ec59301b758fc1f99530677c1947cc1b3fc670a9
RAYCHEV <nikolay.g.raychev@gmail.com>
2017-01-11 20:18:57 +0000

    delete non useful files

--- webhome/fanoff1.py
+++
@@ -1,8 +0,0 @@
-import time
-import RPi.GPIO as GPIO
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(12, GPIO.OUT)
-
-print("fan 1 off")
-
-GPIO.cleanup()
--- webhome/fanoff2.py
+++
@@ -1,8 +0,0 @@
-import time
-import RPi.GPIO as GPIO
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(16, GPIO.OUT)
-
-print("fan 2 off")
-
-GPIO.cleanup()
